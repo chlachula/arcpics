aeab8096a4fc7ac5b4f42a347afcceb1b9cac110
Josef <josefch@gmail.com>
2023-03-30 22:10:43 -0500

    introducing json

--- arcpics.go
+++ arcpics.go
@@ -1,6 +1,7 @@
 package arcpics
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -10,37 +11,25 @@ import (
 	"github.com/boltdb/bolt"
 )
 
+type JfileType = struct {
+	Name    string
+	Time    string
+	Comment string
+}
+type JdirType = struct {
+	Description string
+	Location    string
+	Files       []JfileType
+}
+
 var SYSTEM_BUCKET = []byte("SYSTEM")
 var FILES_BUCKET = []byte("FILES")
 var INIT_LABEL_KEY = "ARC-PICS-LABEL-KEY"
 var defaultPicturesDirName = "Arc-Pics"
 var defaultDatabaseDirName = "DB"
 var defaultArcpicsDbLabel = "arcpics-db-label."
+var jsonFilePrefix = "sample.json"
 
-func DbLabel(archiveDir string) (string, error) {
-	nameStart := defaultArcpicsDbLabel
-	label := ""
-	files, err := os.ReadDir(archiveDir)
-	if err != nil {
-		log.Fatal(err)
-	}
-	count := 0
-	for _, f := range files {
-		if strings.HasPrefix(f.Name(), nameStart) {
-			count++
-			label = f.Name()[len(nameStart):]
-		}
-	}
-	if count == 0 {
-		return label, fmt.Errorf("there is no file %s* at directory %s - should be e.g. %s001", nameStart, archiveDir, nameStart)
-	} else if count > 1 {
-		return label, fmt.Errorf("unexpected number files %s* at directory %s", nameStart, archiveDir)
-	}
-	if len(label) < 1 {
-		return label, fmt.Errorf("there is not at least one character label after dot 'arcpics-db-label.'")
-	}
-	return label, nil
-}
 func picturesAndDatabaseDirectories(args []string) (string, string) {
 	picturesDirName := defaultPicturesDirName
 	databaseDirName := defaultDatabaseDirName
@@ -89,12 +78,43 @@ func insertNewBucket(db *bolt.DB, bucket []byte) {
 		panic(err)
 	}
 }
+
+////////////////////////
+// Exported functions //
+////////////////////////
+
+func DbLabel(archiveDir string) (string, error) {
+	nameStart := defaultArcpicsDbLabel
+	label := ""
+	files, err := os.ReadDir(archiveDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	count := 0
+	for _, f := range files {
+		if strings.HasPrefix(f.Name(), nameStart) {
+			count++
+			label = f.Name()[len(nameStart):]
+		}
+	}
+	if count == 0 {
+		return label, fmt.Errorf("there is no file %s* at directory %s - should be e.g. %s001", nameStart, archiveDir, nameStart)
+	} else if count > 1 {
+		return label, fmt.Errorf("unexpected number files %s* at directory %s", nameStart, archiveDir)
+	}
+	if len(label) < 1 {
+		return label, fmt.Errorf("there is not at least one character label after dot 'arcpics-db-label.'")
+	}
+	return label, nil
+}
+
 func PutDbValue(db *bolt.DB, bucket []byte, key, value string) error {
 	err := db.Update(func(tx *bolt.Tx) error {
 		return tx.Bucket(bucket).Put([]byte(key), []byte(value))
 	})
 	return err
 }
+
 func GetDbValue(db *bolt.DB, bucket []byte, key string) string {
 	var valueBytes []byte
 	_ = db.View(func(tx *bolt.Tx) error {
@@ -138,4 +158,43 @@ func AssignPicturesDirectoryWithDatabase(args []string) (string, *bolt.DB, error
 
 	}
 	return picturesDirName, db, nil
+}
+
+func CreateDirJson(d string, dirFiles JdirType) error {
+	jsonBytes, err := json.Marshal(dirFiles)
+	if err != nil {
+		return err
+	}
+	fname := filepath.Join(d, jsonFilePrefix+"-new")
+	f, err := os.Create(fname)
+	if err != nil {
+		return err
+	}
+	_, err = f.Write(jsonBytes) //lenght ommited
+	if err != nil {
+		return err
+	}
+	f.Close()
+	return nil
+}
+func UpdateDirJson(d string) error {
+	fjson_new := filepath.Join(d, jsonFilePrefix+"-new")
+	if _, err := os.Stat(fjson_new); err != nil {
+		return nil // there is no new file
+	}
+	fjson := filepath.Join(d, jsonFilePrefix)
+	var fjson_FileInfo os.FileInfo
+	var err error
+	if fjson_FileInfo, err = os.Stat(fjson); err != nil {
+		return nil // there is now current file
+	}
+	timeExtension := fjson_FileInfo.ModTime().Format("--2006-01-02_15-04-05") // Mon Jan 2 15:04:05 -0700 MST 2006
+	fjson_time := filepath.Join(d, jsonFilePrefix+timeExtension)
+	if err := os.Rename(fjson, fjson_time); err != nil {
+		return err
+	}
+	if err := os.Rename(fjson_new, fjson); err != nil {
+		return err
+	}
+	return nil
 }
